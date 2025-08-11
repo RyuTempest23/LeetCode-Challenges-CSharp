decc836f098bb35dbbd519512e7176aa541ffbaf
RyuTempest23 <johnvencies@gmail.com>
2025-08-11 03:19:33 -0400

    Update RomanToInteger.cs

--- LeetCode/Easy/RomanToInteger.cs
+++ LeetCode/Easy/RomanToInteger.cs
@@ -2,8 +2,12 @@ public class Solution {
     public int RomanToInt(string s) {
         Dictionary<char, int> numerals = new Dictionary<char, int>()
         {
-            {'I', 1}, {'V', 5}, {'X', 10},
-            {'L', 50}, {'C', 100}, {'D', 500},
+            {'I', 1}, 
+            {'V', 5}, 
+            {'X', 10},
+            {'L', 50}, 
+            {'C', 100}, 
+            {'D', 500},
             {'M', 1000}
         };
 
